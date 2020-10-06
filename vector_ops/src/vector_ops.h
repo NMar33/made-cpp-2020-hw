d8db92f69f6a031f9fec5361032cc647c6f7b123
NMar33 <nmarnautov@gmail.com>
2020-10-06 22:35:52 +0300

    201006_1

--- vector_ops/src/vector_ops.h
+++ vector_ops/src/vector_ops.h
@@ -2,10 +2,140 @@
 #include <vector>
 #include <iostream>
 
+using std::vector;
 
 namespace task {
 
 // Your code here...
+vector<double> operator+ (
+    const vector<double>& a,
+    const vector<double>& b
+){
+    vector<double> c;
 
+    for(int i = 0; i < a.size(); ++ i) {
+        c.push_back(a[i] + b[i]);
+    }
+
+    return c;
+    
+}
+
+vector<double> operator- (
+    const vector<double>& a,
+    const vector<double>& b
+){
+    vector<double> c;
+
+    for(int i = 0; i < a.size(); ++ i) {
+        c.push_back(a[i] - b[i]);
+    }
+
+    return c;
+    
+}
+
+vector<double> operator+ (
+    const vector<double>& a
+){
+    vector<double> c;
+
+    for(int i = 0; i < a.size(); ++ i) {
+        c.push_back(a[i] );
+    }
+
+    return c;
+    
+}
+
+vector<double> operator- (
+    const vector<double>& a
+){
+    vector<double> c;
+
+    for(int i = 0; i < a.size(); ++ i) {
+        c.push_back(a[i] * (-1));
+    }
+
+    return c;
+    
+}
+
+double operator* (
+    const vector<double>& a,
+    const vector<double>& b
+){
+    double c;
+
+    for(int i = 0; i < a.size(); ++ i) {
+        c += a[i] * b[i];
+    }
+
+    return c;
+    
+}
+
+vector<double> operator% (
+    const vector<double>& a,
+    const vector<double>& b
+){
+    vector<double> c;
+
+    c.push_back(a[1] * b[2] - a[2] * b[1]);
+    c.push_back(a[2] * b[0] - a[0] * b[2]);
+    c.push_back(a[0] * b[1] - a[1] * b[0]);
+
+    return c;
+    
+}
+
+bool operator|| (
+    const vector<double>& a,
+    const vector<double>& b
+){
+    double k;
+
+    for(int i = 0; i < a.size(); ++ i) {
+        if ((a[i] != 0) && (b[i] != 0)) {
+            k = a[i] / b[i];
+            break;
+        }
+    }
+    
+     for(int i = 0; i < a.size(); ++ i) {
+        if (a[i] != b[i] * k) {
+            return false;
+        }   
+
+    }
+
+    return true;
+}
+
+bool operator&& (
+    const vector<double>& a,
+    const vector<double>& b
+){
+    double k;
+
+    for(int i = 0; i < a.size(); ++ i) {
+        if ((a[i] != 0) && (b[i] != 0)) {
+            k = a[i] / b[i];
+            break;
+        }
+    }
+    if (k < 0) {
+        return false;
+    }
+
+     for(int i = 0; i < a.size(); ++ i) {
+        if (a[i] != b[i] * k) {
+            return false;
+        }   
+
+    }
+
+    return true;
+}
 
 }  // namespace task
